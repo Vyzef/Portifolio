6010f569d51278bbc561a5262eac396afca11994
Vyzef <ol.sa.gabriel02@gmail.com>
2021-02-12 01:21:15 -0300

    Adds except, try, else clause

--- portfolio-projects/Python/Simple/Number Guesser/guessing.py
+++ portfolio-projects/Python/Simple/Number Guesser/guessing.py
@@ -8,31 +8,42 @@ import random
 
 #   Gera o número base do jogo, define variaveis
 
-ceiling = random.randint(1, 350)
-number = random.randint(1, ceiling)
 
-count = 1
-chute = 0
-
-print("Mano, me fala um número entre 1 e {}, você tem 7 chances." .format(ceiling))
 
 #   O jogo inteiro dentro de um loop, onde se o jogador acertar ou passar do numero de tentativas, o jogo para.
 
-while chute != number:
-    chute = int(input())
-    if (chute == number):
-        print("Nossa, eu real não esperava por essa. Boa mano, você conseguiu, o número que eu tava pensando era {}." .format(number))
-        break
-    if (count == 4):
-        print("Cuidado em, pensa direito que você passou da metade das suas tentavivas...")
-    if (count == 6):
-        print("Ultima tentativa, usa todo esse seu cérebro ai.")
-    if (count == 7):
-        print("Me desculpa mano, mas você nao conseguiu adivinhar o número. Era {}. Tenta de novo e pensa melhor na próxima." .format(number))
-        break
-    else:
-        if (chute < number):
-            print("Jogou muito baixo. Tenta um número maior.")
+def guess():
+
+    ceiling = random.randint(1, 350)
+    number = random.randint(1, ceiling)
+
+    print("Mano, me fala um número entre 1 e {}, você tem 7 chances." .format(ceiling))
+
+    count = 1
+    chute = 0
+    acertou = False
+    
+    while not acertou:
+        try:
+            chute = int(input())
+        except ValueError:
+            print("Acho que você não me entendeu, isso não é um número, e estamos falando deles.")
+            print("Tenta de novo.")
         else:
-            print("Foi lá pro alto. Tenta mais baixo.")
-        count = count + 1
+            if (chute == number):
+                acertou = True
+            elif (count == 7):
+                break
+            if (count == 4):
+                print("Cuidado em, pensa direito que você passou da metade das suas tentavivas...")
+            if (count == 6):
+                print("Ultima tentativa, usa todo esse seu cérebro ai.")
+
+            else:
+                if (chute < number):
+                    print("Jogou muito baixo. Tenta um número maior.")
+                else:
+                    print("Foi lá pro alto. Tenta mais baixo.")
+                count = count + 1
+
+guess()
