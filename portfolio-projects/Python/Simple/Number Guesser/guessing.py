dca14c79cd389cfa6e7971b70d516f90317503cb
Vyzef <ol.sa.gabriel02@gmail.com>
2021-02-12 22:17:56 -0300

    Adds comments, finalizes the code

--- portfolio-projects/Python/Simple/Number Guesser/guessing.py
+++ portfolio-projects/Python/Simple/Number Guesser/guessing.py
@@ -14,6 +14,9 @@ import random
 
 def guess():
 
+    global acertou          #   Define o resultado, se o jogador acertou ou não, como global
+
+    #   Define a diferença maxima, que é onde o numero random vai ser criado
     ceiling = random.randint(1, 350)
     number = random.randint(1, ceiling)
 
@@ -22,7 +25,9 @@ def guess():
     count = 1
     chute = 0
     acertou = False
-    
+        
+    #   O loop funciona enquanto o bool acertou for falso, que só é mudado quando o chute, que seria o input do jogador, é
+    # igual ao numero escolhido
     while not acertou:
         try:
             chute = int(input())
@@ -32,18 +37,31 @@ def guess():
         else:
             if (chute == number):
                 acertou = True
+                break
+            #   Se a acontagem, que é encrementada a cada chute, chegar a 7, 
             elif (count == 7):
                 break
             if (count == 4):
                 print("Cuidado em, pensa direito que você passou da metade das suas tentavivas...")
             if (count == 6):
                 print("Ultima tentativa, usa todo esse seu cérebro ai.")
+                count = count + 1
 
-            else:
+            else:       #   Dependendo do chute, define a dica, e encrementa a contagem
                 if (chute < number):
                     print("Jogou muito baixo. Tenta um número maior.")
                 else:
                     print("Foi lá pro alto. Tenta mais baixo.")
                 count = count + 1
 
+def msg ():         #   Mensagem que é chama depois da finalização do jogo, depende do valor do bool acertou
+    if acertou:
+        print("Mano, eu real não esperava. Parabéns por acertar o número.")
+    else:
+        print("Infelizmente você não conseguiu acertar o número mano, tenta melhor na próxima.")
+
+
 guess()
+msg()
+
+print(input("Obrigado por jogar, precione Enter para fechar."))     #   Input para manter o terminal aberto
