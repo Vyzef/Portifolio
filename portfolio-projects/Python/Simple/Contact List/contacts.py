975dd386090826d20b83827c3bd8fc15d0cca2f0
Vyzef <ol.sa.gabriel02@gmail.com>
2021-02-27 16:12:46 -0300

    Adds Command 2 to contact list, WIP of 3

--- portfolio-projects/Python/Simple/Contact List/contacts.py
+++ portfolio-projects/Python/Simple/Contact List/contacts.py
@@ -12,7 +12,7 @@ def contactList():
 
     while run:
         try:
-            key = int(input("Esperando Resposta:"))
+            key = int(input("Esperando Comando:"))
         except ValueError:
             print("O valor digitado não consta na nossa base de dados.")
             time.sleep(1)
@@ -25,10 +25,46 @@ def contactList():
                 print("Próximo comando...")
                 continue
             if key == 2:
-                print("2")
+                time.sleep(1)
+                print("Você deseja adicionar um contato.")
+                addName = input("Por favor, digite o nome de seu contato desejado.")
+                print(addName)
+                time.sleep(1)
+                try:
+                    addNumber = int(input("Agora por favor, digite o número de seu contato."))
+                except ValueError:
+                    print("O valor digitado não consta como um número, tente novamente.")
+                else:
+                    print("O nome e número a serem adicionados são, respectivamente, {} e {}" .format(addName, addNumber))
+                    addFinal = input("Está correto? Se sim, digite Sim.")
+                    if addFinal == "Sim":
+                        list[addName] = addNumber
+                        print("Seu contato foi adicionado.")
+                        time.sleep(1)
+                        print("Próximo comando...")
+                        continue
+                    else:
+                        print("Se seu numero estava incorreto, por favor, tente novamente.")
+                        print("Próximo comando...")
+                        continue
                 continue
-            if key == 3:
-                print("3")
+            if key == 3: # Por agoram deletar ainda não funciona, por conta de um erro na linha 56
+                time.sleep(1)
+                delete = input("Selecione de 0 a {}, este contato sera deletado." .format(len(list)))
+                print(delete)
+                time.sleep(2)
+                confirm = (input("O contato " + list.get(delete) +  " sera deletado. Por favor, digite Sim para confirmar a remoção, e qualquer outra coisa para abortar."))
+                print(confirm)
+                if confirm == "Sim":
+                    del list[list.get(delete)]
+                    print("O contato desejado sera deletado...")
+                    time.sleep(2)
+                    print("Contato deletado.")
+                    print("Próximo comando...")
+                    continue
+                else: 
+                    print("O processo foi abortado. Por favor, insira a proxima ação.")
+                    print("Próximo comando...")
                 continue
             if key == 4:
                 run = False
@@ -43,7 +79,7 @@ def contactList():
                 break
 
 def exit():
-    print = input("Aperte Enter para sair.")
+    input("Aperte Enter para sair.")
 
 contactList()
 exit()
