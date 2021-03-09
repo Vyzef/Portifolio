853dfc58a4501a52fa67de2fcbd95e67fa9b137b
Vyzef <ol.sa.gabriel02@gmail.com>
2021-03-09 15:15:08 -0300

    Finalizes Binary Search Project

--- portfolio-projects/Python/Simple/Binary Search Algorithm/binary.py
+++ portfolio-projects/Python/Simple/Binary Search Algorithm/binary.py
@@ -1,5 +1,34 @@
-from math import radians
 import random
 
 randomList = sorted((random.sample(range(0, 101), 25)))
-input("Digite o número que você deseja encontrar")
+aim = int(input("Digite o número que você deseja encontrar "))
+exist = False
+
+def search(list, aim):
+    global exist
+    start = 0
+    end = len(randomList) - 1
+
+    while start <= end:
+        midpoint = start + (end - start)//2
+        midvalue = list[midpoint]
+
+        if midvalue == aim:
+            exist = True
+            return midpoint
+        elif aim < midvalue:
+            end = midpoint - 1
+        else:
+            start = midpoint +1
+    return None
+        
+
+def main():
+    print(randomList)
+    if exist:
+        print("Seu item estava na posição " + search(randomList, aim))
+    else:
+        print("Seu item não estava na lista gerada.")
+    input("Aperte enter para fechar a aplicação.")
+
+main()
