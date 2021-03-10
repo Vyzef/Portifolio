d14b0fa1aa88c31848acdae5032fad487a31a58f
Vyzef <ol.sa.gabriel02@gmail.com>
2021-03-10 16:11:23 -0300

    Finishes the Desktop Notification Project Adds comments to Contact List Project

--- portfolio-projects/Python/Simple/contacts.py
+++ portfolio-projects/Python/Simple/contacts.py
@@ -9,28 +9,28 @@ import time
 def contactList():
 
     list = {"Evan Van Gloss": "4445-7856", "Arthur Severo": "5419-6459", "Thiago Fritz": "9476-5488", 
-    "Samuel Jonas": "7674-1846", "Erin Parker": "4876-8438"}
+    "Samuel Jonas": "7674-1846", "Erin Parker": "4876-8438"}        # The dictionary which will be read
 
     print("Obrigado por usar nossos serviços. Digite 1, para verificar a lista de contatos, 2 para adicionar e 3 \n para remover um dos contatos. Digite 4 para sair.")
     time.sleep(1)
     
-    run = True
+    run = True                                                      # While this boolean is true, the loop will run
 
     while run:
         try:
-            key = int(input("Esperando Comando:"))
-        except ValueError:
+            key = int(input("Esperando Comando:"))                  # The key decides which command to run, but must be
+        except ValueError:                                          # an interger, in the case it isn't, the exception will run and restart the loop
             print("O valor digitado não consta na nossa base de dados.")
             time.sleep(1)
             print("Por favor, digite um dos números indicados.")
         else:
-            if key == 1:
+            if key == 1:                                            # With key == 1, the list above will be printed
                 print("Contatos Carregando")
                 time.sleep(2)
                 print(list)
                 print("Próximo comando...")
                 continue
-            if key == 2:
+            if key == 2:                                            # If key == 2, a contact can be added to the instance
                 time.sleep(1)
                 print("Você deseja adicionar um contato.")
                 addName = input("Por favor, digite o nome de seu contato desejado.")
@@ -54,26 +54,26 @@ def contactList():
                         print("Próximo comando...")
                         continue
                 continue
-            if key == 3: # Por agoram deletar ainda não funciona, por conta de um erro na linha 56
-                time.sleep(1)
-                delete = input("Selecione de 0 a {}, este contato sera deletado." .format(len(list)))
-                print(delete)
-                time.sleep(2)
-                confirm = (input("O contato " + list.get(delete) +  " sera deletado. Por favor, digite Sim para confirmar a remoção, e qualquer outra coisa para abortar."))
-                print(confirm)
-                if confirm == "Sim":
-                    del list[list.get(delete)]
-                    print("O contato desejado sera deletado...")
-                    time.sleep(2)
-                    print("Contato deletado.")
-                    print("Próximo comando...")
-                    continue
-                else: 
-                    print("O processo foi abortado. Por favor, insira a proxima ação.")
-                    print("Próximo comando...")
-                continue
-            if key == 4:
-                run = False
+            # if key == 3: # Delete is WIP, there is an error in line 56
+            #     time.sleep(1)
+            #     delete = input("Selecione de 0 a {}, este contato sera deletado." .format(len(list)))
+            #     print(delete)
+            #     time.sleep(2)
+            #     confirm = (input("O contato " + list.get(delete) +  " sera deletado. Por favor, digite Sim para confirmar a remoção, e qualquer outra coisa para abortar."))
+            #     print(confirm)
+            #     if confirm == "Sim":
+            #         del list[list.get(delete)]
+            #         print("O contato desejado sera deletado...")
+            #         time.sleep(2)
+            #         print("Contato deletado.")
+            #         print("Próximo comando...")
+            #         continue
+            #     else: 
+            #         print("O processo foi abortado. Por favor, insira a proxima ação.")
+            #         print("Próximo comando...")
+            #     continue
+            if key == 4:                                            # If key == 4, the program will close itself by
+                run = False                                         # modifying the boolean run
                 print("Obrigado por usar nossos serviços.")
                 time.sleep(1)
                 print(".")
